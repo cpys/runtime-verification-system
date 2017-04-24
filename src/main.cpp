c450dfa2052684fa2a8520310039f8b71267d3ad
chenkuan <chen1511@foxmail.com>
2017-04-24 16:05:55 +0800

    添加静态库链接，尚未成功

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,36 @@
 
 #include <iostream>
 #include "Module.h"
+#include "../lib/api/c++/z3++.h"
+
+using namespace z3;
+
+void demorgan() {
+	std::cout << "de-Morgan example\n";
+
+	context c;
+
+	expr x = c.bool_const("x");
+	expr y = c.bool_const("y");
+	expr conjecture = (!(x && y)) == (!x || !y);
+
+	solver s(c);
+	// adding the negation of the conjecture as a constraint.
+	s.add(!conjecture);
+	std::cout << s << "\n";
+	std::cout << s.to_smt2() << "\n";
+	switch (s.check()) {
+		case unsat:
+			std::cout << "de-Morgan is valid\n";
+			break;
+		case sat:
+			std::cout << "de-Morgan is not valid\n";
+			break;
+		case unknown:
+			std::cout << "unknown\n";
+			break;
+	}
+}
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -21,6 +51,8 @@ int main(int argc, char **argv) {
 		std::cout << "------------state " << i << "----------" << std::endl;
 		std::cout << state_path[i] << std::ends;
 	}
+
+	demorgan();
 
 	return 0;
 }
