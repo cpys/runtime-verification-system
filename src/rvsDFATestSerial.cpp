14c1339fe5f5d16440d36adc7a514b6b6b5ced48
chenzhifengla <chen1511@foxmail.com>
2017-06-27 00:11:58 +0800

    将串口消息连接到验证系统上

---
+++ src/rvsDFATestSerial.cpp
@@ -0,0 +1,95 @@
+//
+// Created by yingzi on 2017/6/26.
+//
+
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <cstring>
+#include <unistd.h>
+#include <csignal>
+#include <sstream>
+#include <Module.h>
+#include <DFAModule.h>
+#include <tinyxml2.h>
+#include <SerialForward.h>
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+
+void quit(int);
+
+SerialForward serialForwardVM("/dev/pts/1");
+SerialForward serialForwardPort("/dev/ttyS0");
+
+int main() {
+    // 建立模型
+    Module* module = new DFAModule();
+
+    module->addVarDecl("int", "x");
+
+    module->addState(1, {"x >= 0", "x < 10"});
+    module->addState(2, {"x >= 10", "x < 20"});
+    module->addState(3, {"x >= 20", "x < 30"});
+    module->addState(4, {"x >= 30", "x < 40"});
+    module->addState(5, {"x >= 40", "x < 50"});
+
+    module->addTran("increase", 1, 2, {"x >= 10"});
+    module->addTran("increase", 2, 3, {"x >= 20"});
+    module->addTran("increase", 3, 4, {"x >= 30"});
+    module->addTran("increase", 4, 5, {"x >= 40"});
+    module->addTran("increase", 1, 3, {"x >= 10"});
+    module->addTran("increase", 2, 4, {"x >= 20"});
+    module->addTran("increase", 3, 5, {"x >= 30"});
+    module->addTran("decrease", 3, 1, {"x < 20"});
+    module->addTran("decrease", 4, 2, {"x < 30"});
+    module->addTran("decrease", 5, 3, {"x < 40"});
+
+    module->initModule();
+
+    module->addSpec({"x3 - x1 <= 20", "x5 - x3 <= 20"});
+
+    signal(SIGINT, quit);
+
+    // 初始化串口监控
+    serialForwardVM.init();
+    serialForwardPort.init();
+
+    // 循环从串口获取消息解析成事件
+    using namespace tinyxml2;
+    XMLDocument xmlDocument;
+    string message;
+    while (true) {
+        message = serialForwardVM.recvMessage();
+        cout << message << endl;
+
+        XMLError xmlError = xmlDocument.Parse(message.c_str());
+        if (xmlError != XML_SUCCESS) {
+            cerr << "Cannot parse " << message << endl;
+            continue;
+        }
+
+        XMLElement* root = xmlDocument.FirstChildElement();
+        string eventName = root->Attribute("name");
+        string eventValue = root->Attribute("value");
+
+        std::istringstream iss(eventValue);
+        string eventVar, eventVarOper, eventVarValue;
+        iss >> eventVar >> eventVarOper >> eventVarValue;
+
+        if (module->addEvent(eventName, {{eventVar, eventVarValue}})) {
+            serialForwardPort.sendMessage(message);
+        }
+    }
+
+    return 0;
+}
+
+void quit(int signum) {
+    serialForwardVM.closePort();
+    serialForwardPort.closePort();
+    exit(0);
+}
---
+++ src/serialForward/SerialForward.cpp
@@ -0,0 +1,173 @@
+//
+// Created by yingzi on 2017/6/26.
+//
+
+#include "SerialForward.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::string;
+using std::vector;
+
+bool SerialForward::init() {
+    if (!setPort()) {
+        cerr << "set " << port << " failed!" << endl;
+        return false;
+    }
+    if (!openPort()) {
+        cerr << "open " << port << " failed!" << endl;
+        return false;
+    }
+    return true;
+}
+
+void SerialForward::closePort() {
+    close(fd);
+}
+
+bool SerialForward::setPort() {
+    struct termios options;
+    if (tcgetattr(fd, &options) != 0) {
+        cerr << "SetupSerial failed!" << endl;
+        return false;
+    }
+
+    vector<int> baudRates{38400, 19200, 9600, 4800, 2400, 1200, 300, 38400, 19200, 9600, 4800, 2400, 1200, 300};
+    vector<int> baudRatesBin{B38400, B19200, B9600, B4800, B2400, B1200, B300, B38400, B19200, B9600, B4800, B2400, B1200, B300};
+
+    for (auto it = baudRates.cbegin(), itb = baudRatesBin.cbegin(); it != baudRates.cend(); ++it, ++itb) {
+        if (*it == baudRate) {
+            cfsetispeed(&options, *itb);
+            cfsetospeed(&options, *itb);
+        }
+    }
+
+    options.c_cflag |= CLOCAL;
+    options.c_cflag |= CREAD;
+    switch(parityBit){
+        case 0 :
+            options.c_cflag &= ~CRTSCTS;
+            break;
+        case 1 :
+            options.c_cflag |= CRTSCTS;
+            break;
+        case 2 :
+            options.c_cflag |= IXON | IXOFF | IXANY;
+            break;
+    }
+
+    options.c_cflag &= ~CSIZE;
+    switch (dataBit){
+        case 5 :
+            options.c_cflag |= CS5;
+            break;
+        case 6	:
+            options.c_cflag |= CS6;
+            break;
+        case 7	:
+            options.c_cflag |= CS7;
+            break;
+        case 8:
+            options.c_cflag |= CS8;
+            break;
+        default:
+            cerr << "Unsupported data size" << endl;
+            return false;
+    }
+
+    switch (parity) {
+        case 'n':
+        case 'N':
+            options.c_cflag &= ~PARENB;
+            options.c_iflag &= ~INPCK;
+            break;
+        case 'o':
+        case 'O':
+            options.c_cflag |= (PARODD | PARENB);
+            options.c_iflag |= INPCK;
+            break;
+        case 'e':
+        case 'E':
+            options.c_cflag |= PARENB;
+            options.c_cflag &= ~PARODD;
+            options.c_iflag |= INPCK;
+            break;
+        case 's':
+        case 'S':
+            options.c_cflag &= ~PARENB;
+            options.c_cflag &= ~CSTOPB;
+            break;
+        default:
+            cerr << "Unsupported parity" << endl;
+            return false;
+    }
+
+    switch (stopBit){
+        case 1:
+            options.c_cflag &= ~CSTOPB;
+            break;
+        case 2:
+            options.c_cflag |= CSTOPB;
+            break;
+        default:
+            cerr << "Unsupported stop bits" << endl;
+            return false;
+    }
+    options.c_oflag &= ~OPOST;
+
+    options.c_cc[VTIME] = 1;
+    options.c_cc[VMIN] = 1;
+
+    tcflush(fd,TCIFLUSH);
+
+    if (tcsetattr(fd, TCSANOW, &options) != 0) {
+        cerr << "com set error" << endl;
+        return false;
+    }
+    return true;
+}
+
+bool SerialForward::openPort() {
+    fd = open(port.c_str(), O_RDWR|O_NOCTTY|O_NDELAY);
+    if (fd < 0){
+        cerr << "Can't Open Serial Port" << port << endl;
+        return false;
+    }
+    if(fcntl(fd, F_SETFL, 0) < 0){
+        cerr << "fcntl failed!" << endl;
+        return false;
+    }
+    if(0 == isatty(STDIN_FILENO)){
+        cerr << port << " is not a terminal device" << endl;
+        return false;
+    }
+    return true;
+}
+
+string SerialForward::recvMessage() {
+    static const int maxBufSize = 1024;
+
+    int buffer[maxBufSize] = {0};
+    int len = read(fd, buffer, maxBufSize);
+    return string(std::begin(buffer), std::begin(buffer) + len);
+}
+
+bool SerialForward::sendMessage(const string &message) {
+
+    if (write(fd, message.c_str(), message.size()) == message.size()) return true;
+    else {
+        tcflush(fd, TCOFLUSH);
+        return false;
+    }
+}
+
